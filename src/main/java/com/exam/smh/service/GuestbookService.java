0f2acdc263aa4d6e1adc1200f01dc07e666a9ebf
Smmmhhh <kua0408@gmail.com>
2024-09-22 15:40:37 +0900

    GuestBook 등록 코드 작성

--- src/main/java/com/exam/smh/service/GuestbookService.java
+++ src/main/java/com/exam/smh/service/GuestbookService.java
@@ -1,4 +1,21 @@
 package com.exam.smh.service;
 
+import com.exam.smh.dto.GuestbookDTO;
+import com.exam.smh.entity.GuestBook;
+
 public interface GuestbookService {
+    /**
+     * 방명록을 등록한다.
+     */
+    Long register(GuestbookDTO dto);
+
+    default GuestBook dtoToEntity(GuestbookDTO dto) {
+        GuestBook entity = GuestBook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
 }
--- src/main/java/com/exam/smh/service/GuestbookServiceImpl.java
+++ src/main/java/com/exam/smh/service/GuestbookServiceImpl.java
@@ -1,4 +1,37 @@
 package com.exam.smh.service;
 
-public class GuestbookServiceImpl {
+import com.exam.smh.dto.GuestbookDTO;
+import com.exam.smh.entity.GuestBook;
+import com.exam.smh.repotsitory.GuestbookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor // 의존성을 자동으로 주입한다.
+public class GuestbookServiceImpl implements GuestbookService {
+
+    private final GuestbookRepository repository;
+
+    /**
+     * 방명록을 등록한다.
+     */
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO--------------------");
+        log.info(dto);
+
+        GuestBook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        // GuestBook을 저장한다
+        repository.save(entity);
+
+        return entity.getGno();
+    }
 }
